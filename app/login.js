37f08f685f33bd07150a0aa2e4b31585e4c80116
eriksilver <eriksilver@users.noreply.github.com>
2015-07-22 14:20:46 -0500

    WIP add Profile for users

--- app/login.js
+++ app/login.js
@@ -23,11 +23,15 @@ angular.module('GA_Dashboard')
         email    : $scope.newUser.email,
         password : $scope.newUser.password
       }).then(function(authData) {
+        //?? currentUser picks up only email from authData
         $scope.currentUser = authData.password.email;
+          console.log("$scope.currentUser:",$scope.currentUser);
           console.log("Logged in as:", authData.uid);
           console.log("Authenticated successfully with payload:", authData);
+
         //with successful login, direct to Connect page
         $state.go("connect");
+
         //catch method used for error handling
       }).catch(function(error) {
         switch (error.code) {
@@ -49,14 +53,25 @@ angular.module('GA_Dashboard')
     };
 
 
-    //register method
+    //register user method
     $scope.register = function () {
+      //use Firebase method createUser to add user with email & password credentials
+      //note this registed, NOT authenticated (logged in)
       ref.createUser({
         email    : $scope.newUser.email,
         password : $scope.newUser.password
       }, function(error, userData) {
         if (error) {
-          console.log("Error creating user:", error);
+          switch (error.code) {
+            case "EMAIL_TAKEN":
+            console.log("The new user account cannot be created because the email is already in use.");
+            break;
+            case "INVALID_EMAIL":
+            console.log("The specified email is not a valid email.");
+            break;
+            default:
+            console.log("Error creating user:", error);
+          }
         } else {
           console.log("Successfully created user account with uid:", userData.uid);
         }
