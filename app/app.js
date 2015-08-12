f4be8d70dfa300d4a4ea256e55e2cc04ab047d21
eriksilver <eriksilver@users.noreply.github.com>
2015-08-12 16:29:42 -0500

    WIP refactor to build out user service

--- app/app.js
+++ app/app.js
@@ -3,110 +3,136 @@
 //console.log("appjs before module declared");
 
 var myApp = angular.module('GA_Dashboard', [
-  'ui.router',
-  'firebase',
-  'ngMessages'
+    'ui.router',
+    'firebase',
 ]);
 
 myApp.config(function ($stateProvider, $urlRouterProvider, $locationProvider) {
 
 });
 
-myApp.service('currentUser', ['$log', function ($log) {
+myApp.service('currentUser', ['$log', '$firebaseAuth', '$firebaseArray',
+function ($log, $firebaseAuth, $firebaseArray) {
     $log.info("begin currentUser service");
 
-    // Get a database reference to our users
-    var ref = new Firebase("https://dazzling-torch-1941.firebaseio.com/users");
-    // Attach an asynchronous callback to read the data at our users reference
-    ref.once("value", function(snapshot) {
-      var profileCheck = snapshot.val();
+    this.checkUserProfile = function (authData) {
+        var currentUser = authData.uid;
+        $log.info("begin checkUserProfile");
+        // Get a database reference to our users
+        var ref = new Firebase("https://dazzling-torch-1941.firebaseio.com/users");
 
-      //if user does NOT have a profile; SAVE profile data
-      if (profileCheck.profileExist === false) {
-        // save the user's profile into the database so we can list users,
-        // use them in Security and Firebase Rules, and show profiles
+        // Attach an asynchronous callback to read the data at our users reference
+        ref.once("value", function(snapshot) {
+            var profileCheck = snapshot.child("currentUser").child("profileExist");
+            $log.info("var profileCheck:", profileCheck);
+            //$log.info("var profileCheck.keys:", profileCheck.email);
+            $log.info("currentUser:", currentUser);
 
-        ref.child(authData.uid).set({
-          profileExist: true,
-          provider: authData.provider,
-          email: authData.password.email,
-          name: getName(authData)
-        });
-        // find a suitable name based on the meta info given by each provider
-        function getName(authData) {
-          switch(authData.provider) {
-            case 'password':
-            return authData.password.email.replace(/@.*/, '');
-            case 'twitter':
-            return authData.twitter.displayName;
-            case 'facebook':
-            return authData.facebook.displayName;
-          }
-        } //end getName
-      } //end Profile SAVE
-    }, function (errorObject) {
-      $log.info("The read failed: " + errorObject.code);
-    });
+            //$log.info("var profileCheck.key:", profileCheck.key());
 
-    //if authData is not null; then we have a User logged in via Firebase authentication
-    //Here we assign the logged in user to the currentUser service
-    currentUser.uid = authData.uid;
-    $log.info("service test - current user id:",currentUser.uid);
-    //Console log to confirm a user is logged in
-    $log.info("***User " + authData.uid + " is logged in with " + authData.provider);
 
-  //if authData is null (user is logged out); be explicit with currentUser service
-  } else {
-    currentUser.uid = null;
-    //Console log to confirm user is logged out
-    $log.info("***User is logged out");
-    $log.info("service test - current user id null:",currentUser.uid);
-  
+            //$log.info("var profileCheck.cU.pE:", profileCheck.currentUser.profileExist);
 
-    // var currentUser = {
-    //   uid: null
-    // };
-    //
-    // return currentUser;
+            //if user does NOT have a profile; SAVE profile data
+            if (profileCheck.currentUser.profileExist === false) {
+                // save the user's profile into the database so we can list users,
+                // use them in Security and Firebase Rules, and show profiles
+
+                ref.child(authData.uid).set({
+                    profileExist: true,
+                    provider: authData.provider,
+                    email: authData.password.email,
+                    name: getName(authData)
+                });
+                // find a suitable name based on the meta info given by each provider
+                function getName(authData) {
+                    switch(authData.provider) {
+                        case 'password':
+                        return authData.password.email.replace(/@.*/, '');
+                        case 'twitter':
+                        return authData.twitter.displayName;
+                        case 'facebook':
+                        return authData.facebook.displayName;
+                    } //end switch
+                } //end getName
+            } //end if profileCheck
+
+            //Firebase "ref.once" check, error object result
+        }, function (errorObject) {
+            $log.info("The read failed: " + errorObject.code);
+        }); //end Firebase ref.once
+    }; //end checkUserProfile
+
+    this.userTest = function () {
+        uid: null
+        $log.info("inside var userTest :");
+    };
+
+    //Uses the onAuth() method to listen for changes in user authentication state
+    this.authCheck = function () {
+
+        function authDataCallback(authData) {
+            $log.info("inside authDataCallback :");
+
+            if (authData) {
+
+                //use currentUser service to check if user has logged in before (has a profile)
+                //currentUser.checkUserProfile();
+
+                //if authData is not null; then we have a User logged in via Firebase authentication
+                //Here we assign the logged in user to the currentUser service
+                //currentUser.uid = authData.uid;
+
+                //Console log to confirm a user is logged in
+                $log.info("***User " + authData.uid + " is logged in with " + authData.provider);
+                $log.info("service test - current user id:",currentUser.uid);
+
+            } else {
+                //if authData is null (user is logged out); be explicit with currentUser service
+                //currentUser.uid = null;
+                //Console log to confirm user is logged out
+                $log.info("***User is logged out");
+                //$log.info("service test - current user id null:",currentUser.uid);
+
+            }//end if(authdata)
+
+
+        } //end authDataCallback
+        // Register the callback to be fired every time auth state changes
+        var ref = new Firebase("https://<YOUR-FIREBASE-APP>.firebaseio.com");
+        ref.onAuth(authDataCallback);
+    }; //end authCheck
+
+
     $log.info("end currentUser service:");
-}]);
+}]); //end currentUser service
 
 myApp.run(["$rootScope", "$state", "$log", "currentUser", function ($rootScope, $state, $log, currentUser) {
 
-  // For each user, create a profile object when Registered
-  // Separate when user registers -UID is created
-  // vs When user logs in - authData is created
-  // When User is authorized, check if they have a Profile
-  // If not, save their authdata as a User
+    // For each user, create a profile object when Registered
+    // Separate when user registers -UID is created
+    // vs When user logs in - authData is created
+    // When User is authorized, check if they have a Profile
+    // If not, save their authdata as a User
+    currentUser.userTest();
 
-  //Uses the onAuth() method to listen for changes in user authentication state
-  function authDataCallback(authData) {
-    if (authData) {
+    currentUser.authCheck();
 
-      currentUser.userService();
-
-    } //end If(authdata)
-  } //end authDataCallback
-
-  // Register a callback to fire the abov function every time auth state changes
-  var ref = new Firebase("https://dazzling-torch-1941.firebaseio.com");
-  ref.onAuth(authDataCallback);
-
-  //subscribes to $stateChangeStart to inspect for requireLogin property
-  //default for UI router to fire this at the $rootScope level
-  $rootScope.$on('$stateChangeStart', function (event, toState, toParams) {
-    //checks for data property "requirelogin" (true/false) in the stateprovider
-    var requireLogin = toState.data.requireLogin;
-    //true if currentUser.uid is defined and is not null
-    var currentUserExists = angular.isDefined(currentUser.uid) && currentUser.uid !== null;
-    //true if requireLogin is required and user is logged out (currentUser is null)
-    var shouldRedirectToLogin = requireLogin && !currentUserExists;
-    //if true, direct to login page
-    if (shouldRedirectToLogin) {
-      event.preventDefault();
-      // redirect back to login
-      $state.go("login");
-    }
-  }); //end $stateChangeStart
+    //subscribes to $stateChangeStart to inspect for requireLogin property
+    //default for UI router to fire this at the $rootScope level
+    $rootScope.$on('$stateChangeStart', function (event, toState, toParams) {
+        //checks for data property "requirelogin" (true/false) in the stateprovider
+        var requireLogin = toState.data.requireLogin;
+        //true if currentUser.uid is defined and is not null
+        var currentUserExists = angular.isDefined(currentUser.uid) && currentUser.uid !== null;
+        //true if requireLogin is required and user is logged out (currentUser is null)
+        var shouldRedirectToLogin = requireLogin && !currentUserExists;
+        //if true, direct to login page
+        if (shouldRedirectToLogin) {
+            event.preventDefault();
+            // redirect back to login
+            $state.go("login");
+        }
+    }); //end $stateChangeStart
 
 }]); //end myApp.run
--- app/login.js
+++ app/login.js
@@ -7,140 +7,142 @@ angular.module('GA_Dashboard')
 // }]);
 
 .controller("LoginCtrl", [
-  "$scope", "$firebaseAuth", "$state","$log",
-  function($scope, $firebaseAuth, $state, $log) {
-    $log.log("LoginCtrl ran");
+    "$scope", "$firebaseAuth", "$state","$log","currentUser",
+    function($scope, $firebaseAuth, $state, $log, currentUser) {
+        $log.log("LoginCtrl ran");
 
-    //establish Firebase reference to be used by all functions in the controller
-    var ref = new Firebase("https://dazzling-torch-1941.firebaseio.com/");
-    $scope.authObj = $firebaseAuth(ref);
-
-
-    //login method | submits form after iniital validation has occurred
-    $scope.submitLogin = function (isValid) {
-
-      // check to make sure the form is completely valid
-      if (isValid) {
-
-        $scope.authObj.$authWithPassword({
-          email    : $scope.userLogin.email,
-          password : $scope.userLogin.password
-        }).then(function(authData) {
-          //with successful login, direct to Dashboard page
-          //$state.go("dashboard");
-
-          $log.info("Authenticated successfully with payload:", authData);
-          // //create user profile with login authdata
-          // ref.child("users").child(authData.uid).set({
-          //   email: $scope.userLogin.email,
-          //   name: getName(authData),
-          //   profileExist: true,
-          //   provider: authData.provider, //authentication method, e.g. password
-          // });
-          //
-          // // find a suitable name for the user based on the meta info given by each provider
-          // function getName(authData) {
-          //   switch(authData.provider) {
-          //     case 'password':
-          //     return authData.password.email.replace(/@.*/, '');
-          //     // use below if twitter or facebook authentication is used
-          //     // case 'twitter':
-          //     // return authData.twitter.displayName;
-          //     // case 'facebook':
-          //     // return authData.facebook.displayName;
-          // }
-          // }
-
-
-          //catch method used for error handling
-        }).catch(function(error) {
-
-          $log.info("showError before switch",$scope.showError);
-          switch (error.code) {
-            case "INVALID_EMAIL":
-            $scope.showError=true;
-            $log.error("The specified user account email is invalid.");
-            break;
-            case "INVALID_PASSWORD":
-            $log.error("The specified user account password is incorrect.");
-            break;
-            case "INVALID_USER":
-            $scope.showError=true;
-            $log.error("The specified user account does not exist.");
-            break;
-            default:
-            $log.error("Error logging user in:", error);
-          };
-          $log.info("showError after switch",$scope.showError);
-        });
-
-        $scope.resetForm ();
-      }; //end isValid
-    }; //end submitLogin
-
-
-
-    //login method | submits form after iniital validation has occurred
-    $scope.submitRegister = function (isValid) {
-
-      // check to make sure the form is completely valid
-      if (isValid) {
-
-        $log.info("$scope.userRegister.firstName-1",$scope.userRegister.firstName);
+        //establish Firebase reference to be used by all functions in the controller
         var ref = new Firebase("https://dazzling-torch-1941.firebaseio.com/");
+        $scope.authObj = $firebaseAuth(ref);
 
-        //use Firebase method createUser to add user with email & password credentials
-        //note this only give user a UID, no other info attached to user and NOT authenticated (logged in)
-        ref.createUser({
-          email    : $scope.userRegister.email,
-          password : $scope.userRegister.password
-        }, function(error, userData) {
-          if (error) {
-            switch (error.code) {
-              case "EMAIL_TAKEN":
-              $log.error("The new user account cannot be created because the email is already in use.");
-              break;
-              case "INVALID_EMAIL":
-              $log.error("The specified email is not a valid email.");
-              break;
-              default:
-              $log.error("Error creating user:", error);
-            }
-          } else {
-            //note userData consists solely of UID
-            //this sets up empty user profile - will be filled in when user logs in with authData method
-            ref.child("users").child(userData.uid).set({
-              email: " ",
-              name: " ",
-              profileExist: false,
-              provider: " "
-            });
 
-            //note userData consists solely of UID
-            $log.info("Successfully created user account with uid:", userData.uid);
+        //login method | submits form after initial validation has occurred
+        $scope.submitLogin = function (isValid) {
 
-            //$log.info("$scope.userRegister.firstName-2",$scope.userRegister.firstName);
-          } //end function
-        }); //end createUser
+            // check to make sure the form is completely valid
+            if (isValid) {
 
-      } //end isValid
+                $scope.authObj.$authWithPassword({
+                    email    : $scope.userLogin.email,
+                    password : $scope.userLogin.password
+                }).then(function(authData) {
 
-      $scope.resetRegisterForm ();
-    }; //end submitRegister
+                    currentUser.checkUserProfile(authData);
+                    //with successful login, direct to Dashboard page
+                    //$state.go("dashboard");
 
-    //logout user
-    $scope.logout = function () {
-      ref.unauth();
-    };
+                    $log.info("Authenticated successfully with payload:", authData);
+                    // //create user profile with login authdata
+                    // ref.child("users").child(authData.uid).set({
+                    //   email: $scope.userLogin.email,
+                    //   name: getName(authData),
+                    //   profileExist: true,
+                    //   provider: authData.provider, //authentication method, e.g. password
+                    // });
+                    //
+                    // // find a suitable name for the user based on the meta info given by each provider
+                    // function getName(authData) {
+                    //   switch(authData.provider) {
+                    //     case 'password':
+                    //     return authData.password.email.replace(/@.*/, '');
+                    //     // use below if twitter or facebook authentication is used
+                    //     // case 'twitter':
+                    //     // return authData.twitter.displayName;
+                    //     // case 'facebook':
+                    //     // return authData.facebook.displayName;
+                    // }
+                    // }
 
-    //resets newUser object
-    $scope.resetRegisterForm = function (email, password) {
-      $scope.userRegister = {email: '', password: ''};
-    };
 
-    //resets newUser object
-    $scope.resetForm = function (email,password) {
-      $scope.newUser = {email: '', password: ''};
-    };
+                    //catch method used for error handling
+                }).catch(function(error) {
 
-  }]); //end LoginCtrl
+                    $log.info("showError before switch",$scope.showError);
+                    switch (error.code) {
+                        case "INVALID_EMAIL":
+                        $scope.showError=true;
+                        $log.error("The specified user account email is invalid.");
+                        break;
+                        case "INVALID_PASSWORD":
+                        $log.error("The specified user account password is incorrect.");
+                        break;
+                        case "INVALID_USER":
+                        $scope.showError=true;
+                        $log.error("The specified user account does not exist.");
+                        break;
+                        default:
+                        $log.error("Error logging user in:", error);
+                    };
+                    $log.info("showError after switch",$scope.showError);
+                });
+
+            }; //end isValid
+            $scope.resetForm ();
+
+        }; //end submitLogin
+
+
+
+        //login method | submits form after iniital validation has occurred
+        $scope.submitRegister = function (isValid) {
+
+            // check to make sure the form is completely valid
+            if (isValid) {
+
+                $log.info("$scope.userRegister.firstName-1",$scope.userRegister.firstName);
+                var ref = new Firebase("https://dazzling-torch-1941.firebaseio.com/");
+
+                //use Firebase method createUser to add user with email & password credentials
+                //note this only give user a UID, no other info attached to user and NOT authenticated (logged in)
+                ref.createUser({
+                    email    : $scope.userRegister.email,
+                    password : $scope.userRegister.password
+                }, function(error, userData) {
+                    if (error) {
+                        switch (error.code) {
+                            case "EMAIL_TAKEN":
+                            $log.error("The new user account cannot be created because the email is already in use.");
+                            break;
+                            case "INVALID_EMAIL":
+                            $log.error("The specified email is not a valid email.");
+                            break;
+                            default:
+                            $log.error("Error creating user:", error);
+                        }
+                    } else {
+                        //note userData consists solely of UID
+                        //this sets up empty user profile - will be filled in when user logs in with authData method
+                        ref.child("users").child(userData.uid).set({
+                            email: " ",
+                            name: " ",
+                            profileExist: false,
+                            provider: " "
+                        });
+
+                        //note userData consists solely of UID
+                        $log.info("Successfully created user account with uid:", userData.uid);
+
+                    } //end function
+                }); //end createUser
+
+            } //end isValid
+
+            $scope.resetRegisterForm ();
+        }; //end submitRegister
+
+        //logout user
+        $scope.logout = function () {
+            ref.unauth();
+        };
+
+        //resets newUser object
+        $scope.resetRegisterForm = function (email, password) {
+            $scope.userRegister = {email: '', password: ''};
+        };
+
+        //resets newUser object
+        $scope.resetForm = function (email, password) {
+            $scope.userLogin = {email: '', password: ''};
+        };
+
+    }]); //end LoginCtrl
