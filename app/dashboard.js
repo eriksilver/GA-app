c9940ad0e34a78ca21597367c94e3e4ca0f842d7
eriksilver <eriksilver@users.noreply.github.com>
2015-09-11 14:53:53 -0500

    removed Google Embed API View Selector - pulling data via API is working

--- app/dashboard.js
+++ app/dashboard.js
@@ -8,78 +8,147 @@ angular.module('GA_Dashboard')
   function($scope, $firebaseAuth, $state, $log) {
     $log.log("DashboardCtrl ran");
 
-    // Step 4: Create the view selector.
-    // The View selector component can be used to obtain the ids of a particular view,
-    // so you can run a report for it.
-    // To create a view selector, all you need is the container reference (html id in Step 1).
-    // Note This creates the view selector component, but it doesn't yet render it on the page.
-    // To do that you need to call execute(), which is handled in step 6.
-
-    // $log.info("user authed?", gapi.analytics.auth.isAuthorized());
-
-    ///Google Analytics Auth
+    ///Google Analytics API ready function - waits until API script loads
     gapi.analytics.ready(function() {
-      // Step 3: Authorize the user.
+        // Step 3: Authorize the user.
 
-      var CLIENT_ID = '219791845501-it7kgsija2fr04vvcf2lu7ne6pfq6r7a.apps.googleusercontent.com';
-      //One-click auth button needs passed in a container reference and your client ID
-      gapi.analytics.auth.authorize({
-        container: 'auth-button',
-        clientid: CLIENT_ID,
-      });
+        //Need to define CLIENT ID and SCOPES
+        var CLIENT_ID = '219791845501-it7kgsija2fr04vvcf2lu7ne6pfq6r7a.apps.googleusercontent.com';
+        // Set authorized scope.
+        var SCOPES = ['https://www.googleapis.com/auth/analytics.readonly'];
+        //One-click auth button needs passed in a container reference and your client ID
+        var authData = {
+            container: 'auth-button',
+            clientid: CLIENT_ID,
+            scope: SCOPES,
+        };
+
+        //pass in authorizing data
+        gapi.analytics.auth.authorize(authData,{
+
+        });
+
+        //With successful authorization, log response, and call function
+        gapi.analytics.auth.on('success', function(response) {
+            console.log("auth response",response);
+            runReport();
+            // runChart();
+            // listAccountSummaries();
+        });
+
     });
 
-    $log.info("Is user GA authed?(login.js):", gapi.analytics.auth.isAuthorized());
+    //test using Google Embed API get get report data
+    function runReport() {
+        $log.info("runReport ran");
 
+        var report = new gapi.analytics.report.Data({
+            query: {
+                ids: 'ga:4067996', //Profile ID
+                metrics: 'ga:sessions',
+                dimensions: 'ga:city'
+            }
+        });
 
-    gapi.analytics.ready(function() {
-      // $log.info("gapi ready go - dashboardjs");
+        report.on('success', function(response) {
+            console.log("runReport", response);
+            var reportData = response.query;
+            console.log('reportData',reportData);
+            // var reportDataJSON = JSON.parse(reportData);
+            // console.log('reportDataJSON',reportDataJSON);
 
-    var viewSelector = new gapi.analytics.ViewSelector({
-      container: 'view-selector'
-    });
+        });
+        report.execute();
+    }
 
-    // Step 5: Create the timeline chart.
-    // The Embed API provides you with a chart component that is both a Google chart as well as a report object
-    // in one. This greatly simplifies the process for displaying data as the chart object will run your reports
-    // behind the scene and automatically update itself with the results.
-    // To create a chart component you'll need to specify the API query parameters as well as the chart options.
-    // Within the chart options is a reference to the ID of the container you created in step 1.
-    // Note: As with the view selector, this creates the chart component but to render it to the page you
-    // need to call execute(), which will be explained in the next step
-
-    var timeline = new gapi.analytics.googleCharts.DataChart({
-      reportType: 'ga',
-      query: {
-        'dimensions': 'ga:date',
-        'metrics': 'ga:sessions, ga:pageviews',
-        'start-date': '90daysAgo',
-        'end-date': 'yesterday',
-      },
-      chart: {
-        type: 'LINE',
-        container: 'timeline'
-      }
-    });
-
-    // Step 6: Hook up the components to work together.
+    // //test using Google Embed API to generate a Chart
+    // function runChart() {
+    //     $log.info("runChart ran");
+    //     var chart = new gapi.analytics.googleCharts.DataChart({
+    //         query: {
+    //             ids: 'ga:4067996', //Profile ID
+    //             metrics: 'ga:sessions',
+    //             dimensions: 'ga:date'
+    //         },
+    //         chart: {
+    //             type: 'LINE',
+    //             container: 'line-chart',
+    //             options: {
+    //                 title: 'Sessions over the past week.',
+    //                 fontSize: 12
+    //             }
+    //         }
+    //     });
+    //     chart.on('success', function(response) {
+    //         console.log("chart response", response);
+    //         // response.chart; // the Google Chart instance.
+    //         // response.data : the Google Chart data object.
+    //     });
     //
-    // Embed API components emit events when various things happen such as successful authorization,
-    // selecting a new view, or a chart being fully rendered.
-    //
-    // The example application in this guide waits to render the view selector until after authorization
-    // has happened, and it updates the timeline chart whenever a new view is selected from the view selector
+    //     chart.execute();
+    // }
 
-    viewSelector.execute();
+    function listAccountSummaries() {
+        $log.info("listAccountSummaries ran");
 
-    viewSelector.on('change', function(ids) {
-      var newIds = {
-        query: {
-          ids: ids
+        // Get a list of all Google Analytics accounts for this user
+        var request = gapi.client.analytics.management.accountSummaries.list();
+        request.execute(handleResponse)
+    }
+
+    /*
+    * Example 2:
+    * The results of the list method are passed as the response object.
+    * The following code shows how to iterate through them.
+    */
+    function handleResponse(response) {
+        if (response && !response.error) {
+            if (response.items) {
+                printAccountSummaries(response.items);
+            }
+        } else {
+            console.log('There was an error: ' + response.message);
         }
-      }
-      timeline.set(newIds).execute();
-    });
-  });
+    }
 
-}]);
+
+    function printAccountSummaries(accounts) {
+        for (var i = 0, account; account = accounts[i]; i++) {
+            console.log('Account id: ' + account.id);
+            console.log('Account name: ' + account.name);
+            console.log('Account kind: ' + account.kind);
+
+            // Print the properties.
+            if (account.webProperties) {
+                printProperties(account.webProperties);
+            }
+        }
+    }
+
+
+    function printProperties(properties) {
+        for (var j = 0, property; property = properties[j]; j++) {
+            console.log('Property id: ' + property.id);
+            console.log('Property name: ' + property.name);
+            console.log('Property kind: ' + property.kind);
+            console.log('Internal id: ' + property.internalWebPropertyId);
+            console.log('Property level: ' + property.level);
+            console.log('Property url: ' + property.websiteUrl);
+
+            // Print the views (profiles).
+            if (property.profiles) {
+                printProfiles(property.profiles);
+            }
+        }
+    }
+
+
+    function printProfiles(profiles) {
+        for (var k = 0, profile; profile = profiles[k]; k++) {
+            console.log('Profile id: ' + profile.id);
+            console.log('Profile name: ' + profile.name);
+            console.log('Profile kind: ' + profile.kind);
+            console.log('Profile type: ' + profile.type);
+        }
+    }
+}]); //end DashboardCtrl
