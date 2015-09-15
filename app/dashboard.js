99bfd650ca56386e1469c921a0488224f8cfcb9a
eriksilver <eriksilver@users.noreply.github.com>
2015-09-15 13:25:15 -0500

    Keen stripped out, Chartjs demo is working

--- app/dashboard.js
+++ app/dashboard.js
@@ -66,7 +66,7 @@ angular.module('GA_Dashboard')
             console.log("formattedJson rows", rows);
             console.log("raw rows", response.result.rows);
             reportData = response.result.rows;
-            runKeen();
+            runChart();
 
             // document.getElementById('query-output').value = formattedJson;
             // console.log("formattedJson from runReport", formattedJson);
@@ -77,41 +77,48 @@ angular.module('GA_Dashboard')
         });
     }
 
-    ///////////////////BEGIN KEEN IO SETUP
-function runKeen() {
+
+function runChart() {
     console.log("runKeen-go");
-    var client = new Keen({
-        projectId: "55f3306dc2266c7197951e78", // String (required always)
-        writeKey: "803224c232778760d9165fe80bd5bc18c9658ea85495fefeb951041dc69575b49534cd9e894861be7cd0a119cf94a57f92e9681430acef6c02532d8fd9c01f5fee4ba972ec3ec80f94225e85e2c10c1c2d6f76dcaccf7dbae6086d686478c8aaf7ab997a5f3b0f990374efd9539f09a2",   // String (required for sending data)
-        readKey: "1dfb786587f806c22b1b6240158b02145e8016ddbf4097e3912aaf28497935dcb479b5ae436950c3c17e966fa62b716637000c9618e416cc708b7310d18fb3e9587f7284b9d7a086c063e502fbb3e8628d25cd756f56ae131f37f243f1669006d72743aeb39457e59fa5b805f1f31a4b"      // String (required for querying data)
+    var data = [
+    {
+        value: 300,
+        color:"#F7464A",
+        highlight: "#FF5A5E",
+        label: "Red"
+    },
+    {
+        value: 50,
+        color: "#46BFBD",
+        highlight: "#5AD3D1",
+        label: "Green"
+    },
+    {
+        value: 100,
+        color: "#FDB45C",
+        highlight: "#FFC870",
+        label: "Yellow"
+    },
+    {
+        value: 40,
+        color: "#949FB1",
+        highlight: "#A8B3C5",
+        label: "Grey"
+    },
+    {
+        value: 120,
+        color: "#4D5360",
+        highlight: "#616774",
+        label: "Dark Grey"
+    }
 
-        // protocol: "https",         // String (optional: https | http | auto)
-        // host: "api.keen.io/3.0",   // String (optional)
-        // requestType: "jsonp"       // String (optional: jsonp, xhr, beacon)
-    });
+    ];
 
-    // // Create a data object to record multiple events
-    // var multipleEvents = {
-    //   "purchases": [
-    //     { item: "golden gadget", price: 2550, transaction_id: "f029342" },
-    //     { item: "a different gadget", price: 1775, transaction_id: "f029342" }
-    //   ],
-    //   "transactions": [
-    //     {
-    //       id: "f029342",
-    //       items: 2,
-    //       total: 4325
-    //     }
-    //   ]
-    // };
 
-    // Create a data object to record multiple events
-    var querySimpleViews = {
-            // "time": {"first": "1441123601", "second": "1441469201", "third": "1441901201"},
-            "time": {"first": "2015-09-01T20:39:30.202Z", "second": "2015-09-05T20:39:30.202Z", "third": "2015-09-10T20:39:30.202Z"},
-            "views": {"first": 100, "second": 200, "third": 300}
-
-    };
+    // Get the context of the canvas element we want to select
+    var ctx = document.getElementById("myChart").getContext("2d");
+    var myNewChart = new Chart(ctx).PolarArea(data);
+    new Chart(ctx).PolarArea(data, options);
 
 //NOTE ON DATA FORMAT required
 //A batch of events should be a JSON object, keyed by event collection names,
@@ -121,7 +128,7 @@ function runKeen() {
 
     // var reportDataJSON = JSON.stringify(reportData);
     //alert(reportDataJSON);
-    reportDataJSON = {"data": reportData, "keen": {}} ; //makes reportData an object with "data" key
+    // reportDataJSON = {"data": reportData, "keen": {}} ; //makes reportData an object with "data" key
     //order data by date key?
     //store data by report?
     //might be harder to chart nested data
@@ -132,18 +139,6 @@ function runKeen() {
 
 
 
-    // Send multiple events to several collections
-    client.addEvent("Queries7",querySimpleViews, function(err, res){
-      if (err) {
-          $log.info("add Event = error",err);
-
-        // there was an error!
-      }
-      else {
-          $log.info("add Event = success",res);
-        // see sample response below
-      }
-    });
 }
 
 }]); //end DashboardCtrl
